8aabd1fdcf6e2e73f5bbf70416b4646cc4d1a1b7
Evelyn <evelyn.a.kosma@gmail.com>
2022-01-12 14:49:54 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const mapToNegativize = (arr) => {
+  return arr.map(x => x * -1)
+}
+
+const mapToNoChange = (arr) => {
+  return arr.map(x => x )
+} 
+
+const mapToDouble = (arr) => {
+  return arr.map(x => x * 2 )
+} 
+
+const mapToSquare = (arr) => {
+  return arr.map(x => x * x )
+} 
+
+function reduceToTotal(num, start = 0) {
+  return num.reduce((prev, curr) => prev + curr, start)
+}
+
+const reduceToAllTrue = obj => Boolean(
+  Object.values(obj)
+    .reduce((a, b) => a && b)
+);
+
+function reduceToAnyTrue(array){
+  const reducer = function(prev, currnum){  
+      if(prev == true){
+          return true
+      } else {
+          return !!currnum
+      } 
+  }
+  return array.reduce(reducer, false)
+} 
